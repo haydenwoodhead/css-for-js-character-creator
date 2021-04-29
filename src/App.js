25079b4b58c2d55d2b694106ee8432edd385c241
Hayden Woodhead <hayden@movio.co>
2021-04-29 15:01:16 +1200

    Do exercises

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
+
+import styles from "./App.module.css";
 
 function App() {
   return (
